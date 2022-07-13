ebb8bd47764803d2484c23efe17bc05888d20eb8
vikas7460 <vikaspatel7460@gmail.com>
2022-07-13 18:20:55 +0530

    first

---
+++ Album/metadata.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let axios = require("axios");
+
+let songs = ["JTigera","JTwinkleb"];
+let durations = ["00:15","00:05"];
+let ipfsArray = [];
+
+for (let i = 0; i < songs.length; i++) {
+  ipfsArray.push({
+    path: `metadata/${i}.json`,
+    content: {
+      image: `ipfs://QmNsnqDMmRMKgWm2dQrZ4uFGQNUiZ7vmbfgptYyAgfKNVx/media/2`, //xxx = hash
+      name: songs[i],
+      animation_url: `ipfs://QmNsnqDMmRMKgWm2dQrZ4uFGQNUiZ7vmbfgptYyAgfKNVx/media/${i}`, //xxx = hash
+      duration: durations[i],
+      artist: "/vikas patwl",
+      year: "2022"
+    },
+  });
+}
+
+axios.post("https://deep-index.moralis.io/api/v2/ipfs/uploadFolder", ipfsArray, {
+    headers: {
+      "X-API-KEY":
+        "cpHiPkaPq6INHBpo8mPYEcLJnkVrGb5f887f9Ai8fEHd5cA4PmNTUCSa8HM45D8k",
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+  })
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ Album/music.js
@@ -0,0 +1,38 @@
+let fs = require("fs");
+let axios = require("axios");
+
+let media = ["JTigera.mp3","JTwinkleb.mp3","NonFungiblec.png"];
+let ipfsArray = [];
+let promises = [];
+
+for (let i = 0; i < media.length; i++) {
+  promises.push(
+    new Promise((res, rej) => {
+      fs.readFile(`${__dirname}/export/${media[i]}`, (err, data) => {
+        if (err) rej();
+        ipfsArray.push({
+          path: `media/${i}`,
+          content: data.toString("base64"),
+        });
+        res();
+      });
+    })
+  );
+}
+Promise.all(promises).then(() => {
+  axios
+    .post("https://deep-index.moralis.io/api/v2/ipfs/uploadFolder", ipfsArray, {
+      headers: {
+        "X-API-KEY":
+          "cpHiPkaPq6INHBpo8mPYEcLJnkVrGb5f887f9Ai8fEHd5cA4PmNTUCSa8HM45D8k",
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
